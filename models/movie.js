0fa44356d1faf897e1a1fbf829dd0675d3f01397
Ruslan Bestaev <ruslanbestaev77@yandex.ru>
2022-03-25 18:56:37 +0300

    Подготовил проект, создал схемы

---
+++ models/movie.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String, // имя — это строка
+    required: true,
+  },
+  director: {
+    type: String, // имя — это строка
+    required: true,
+  },
+  year: {
+    type: String, // имя — это строка
+    required: true,
+  },
+  description: {
+    type: String, // имя — это строка
+    required: true,
+  },
+  // ссылка на постер к фильму
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value, { require_protocol: true });
+      },
+      message: 'Ссылка некоректная',
+    },
+  },
+  // ссылка на трейлер фильма
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value, { require_protocol: true });
+      },
+      message: 'Ссылка некоректная',
+    },
+  },
+  // миниатюрное изображение постера к фильму
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value, { require_protocol: true });
+      },
+      message: 'Ссылка некоректная',
+    },
+  },
+
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value, { require_protocol: true });
+      },
+      message: 'Ссылка некоректная',
+    },
+  },
+  // _id пользователя, который сохранил фильм
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  // id фильма, который содержится в ответе сервиса MoviesExplorer
+  /* movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'movie',
+  }, */
+  nameRU: { // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+  nameEN: { // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+  },
+
+});
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcryptjs'); // модуль для хэша
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Некорректный email',
+    },
+  },
+  // должен быть хэш пароля
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: { // у пользователя есть имя — опишем требования к имени в схеме:
+    type: String, // имя — это строка
+    minlength: 2, // минимальная длина имени — 2 символа
+    maxlength: 30, // а максимальная — 30 символов
+    required: true,
+  },
+});
+// добавим метод findUserByCredentials схеме пользователя
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+// попытаемся найти пользователя по почте
+  return this.findOne({ email }).select('+password') // this — это модель User
+    .then((user) => {
+      // не нашёлся — отклоняем промис
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      // нашёлся — сравниваем хеши
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user; // теперь user доступен
+        });
+    });
+};
+module.exports = mongoose.model('user', userSchema);
